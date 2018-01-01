76dcf3d2a0c53f2be5a9acd952d94a9279687529
Artemij Shepelev <ffelian@gmail.com>
2018-01-01 15:24:08 +0300

    writing EP tests

--- piece/pawn_test.go
+++ piece/pawn_test.go
@@ -456,3 +456,32 @@ var _ = Describe("Pawn promotion test", func() {
 		Expect(b.Equals(boardCopy)).To(BeTrue())
 	})
 })
+
+var _ = FDescribe("En passant capturing test", func() {
+	var b base.IBoard
+	resetBoard := func() { b = rect.NewStandardChessBoard() }
+	BeforeEach(func() { resetBoard() })
+
+	It("checks that en passant can't be done (not set in board)", func() {
+		wp, bp := piece.NewPawn(White), piece.NewPawn(Black)
+		b.PlacePiece(rect.Coord{2, 2}, wp)
+		b.PlacePiece(rect.Coord{3, 2}, bp)
+
+		a, d := wp.Attacks(b), wp.Destinations(b)
+		Expect(a.Len()).To(Equal(2))
+		Expect(d.Len()).To(Equal(2))
+		sort.Sort(a)
+		sort.Sort(d)
+		Expect(a.Equals(rect.NewCoords([]base.ICoord{rect.Coord{1, 3}, rect.Coord{3, 3}}))).To(BeTrue())
+		Expect(d.Equals(rect.NewCoords([]base.ICoord{rect.Coord{2, 3}, rect.Coord{2, 4}}))).To(BeTrue())
+
+		a, d = bp.Attacks(b), bp.Destinations(b)
+		Expect(a.Len()).To(Equal(2))
+		Expect(d.Len()).To(Equal(1))
+		sort.Sort(a)
+		sort.Sort(d)
+		Expect(a.Equals(rect.NewCoords([]base.ICoord{rect.Coord{2, 1}, rect.Coord{4, 1}}))).To(BeTrue())
+		Expect(d.Equals(rect.NewCoords([]base.ICoord{rect.Coord{3, 1}}))).To(BeTrue())
+	})
+
+})
